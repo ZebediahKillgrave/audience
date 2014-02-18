6a909abd9777187d36460652df50996394e90b90
Florent Espanet <florent.esp@gmail.com>
2014-02-18 16:32:35 +0100

    reworked the python code, now simpler

--- audience/main.py
+++ audience/main.py
@@ -1,22 +1,23 @@
 from flask import Flask, request, render_template
 import tweepy
-import logging
+from tweepy import TweepError
 from time import time
 from keys import CONSUMER_KEY, CONSUMER_SECRET, USER_KEY, USER_SECRET
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 class TwitterLimit(Exception):
-    """Custom exception handling twiter api reset time for the display"""
+    """
+    Custom exception handling twiter api reset time for the display
+    """
     def __init__(self, message, reset):
         Exception.__init__(self, "%s [reset in %dsec]" % (message, reset - time()))
 
 class RateLimit(object):
-    """This class will check the remaining calls to twitter api on demand and
-    raise a TwitterLimit exception if there is no more call available"""
+    """
+    This class will check the remaining calls to twitter api on demand and
+    raise a TwitterLimit exception if there is no more call available
+    """
     def __init__(self, limits):
         self.resources = limits["resources"]
         self.rate_limit_context = limits["rate_limit_context"]
@@ -27,89 +28,63 @@ class RateLimit(object):
             raise TwitterLimit("Limit reached for %s" % (name), current["reset"])
 
 class AuthHandler(object):
-    """Simple class that will connect us to twitter api when instantiate"""
+    """
+    Simple class that will connect us to twitter api when instantiate
+    """
     def __init__(self, consumer, consumer_secret, token, token_secret):
         self.auth = tweepy.OAuthHandler(consumer, consumer_secret)
         self.auth.set_access_token(token, token_secret)
         self.api = tweepy.API(self.auth)
 
-class FollowRTProc(object):
-    def __init__(self, api, tweetid, organisator):
-        self.tweetid = tweetid
+
+class Tweet(object):
+    def __init__(self, tweetid, user, api):
         self.api = api
-        self.count = 0
-        self.orga = organisator
-        self.users = []
-
-    def add_retweeter(self, user):
-        print "Added user : {}".format(user)
-        self.users.append(user)
-        self.count += user.followers_count
-
-    def run(self):
-        count = self.api.get_status(self.tweetid).retweet_count
-        try:
-            rts = self.api.retweets(self.tweetid)
-        except tweepy.TweepError:
-            pass
-        else:
-            for rt in rts:
-                self.add_retweeter(rt.user)
-        me = self.api.get_user(screen_name=self.orga)
-        self.add_retweeter(me)
-        return self.count
-
-    def print_count(self):
-        json = {
-            "screen_name": [],
-            "followers": []
-            }
-        print "Users : {}".format(self.users)
-        for user in self.users:
-            json["screen_name"].append(user.screen_name)
-            json["followers"].append(user.followers_count)
-        return json
-            
-def process(auth, datas):
-    total = 0
-    json = {
-        "total": 0,
-        "users": {"screen_name": [], "followers": []}
-        }
-    for data in [d for d in datas if d]:
-        limits = RateLimit(auth.api.rate_limit_status())
-        try:
-            limits.check_remaining("statuses", "retweets/:id")
-            limits.check_remaining("users", "show/:id")
-        except TwitterLimit as e:
-            return json, e
-        frt = FollowRTProc(auth.api, data["tweetid"], data["orga"])
-        json["total"] += frt.run()
-        users = frt.print_count()
-        json["users"]["screen_name"] += users["screen_name"]
-        json["users"]["followers"] += users["followers"]
-    return json, None
-
-def parse_url(url):
-    try:
-        tweetid = url.split('/')[-1]
-        orga = url.split('/')[-3]
-    except IndexError:
-        return None
-    return {"tweetid":tweetid, "orga":orga}
+        self.user = user
+        self.tweetid = tweetid
+        self.user_followers = api.get_user(screen_name=user).followers_count
+        self.retweets = api.retweets(tweetid)
     
+    def compute_audience(self):
+        audience = 0
+        for retweet in self.retweets:
+            audience += retweet.user.followers_count
+        return audience + self.user_followers
+
+class URL(object):
+    def __init__(self, url):
+        self.url = url
+        try:
+            self.tweetid = url.split('/')[-1]
+            self.user = url.split('/')[-3]
+        except IndexError:
+            self.tweetid = None
+            self.user = None
+
+class Process(object):
+    def __init__(self, urls, auth):
+        self.urls = map(URL, urls)
+        self.auth = auth
+    
+    def run(self):
+        total = 0
+        for url in self.urls:
+            if url.tweetid and url.user:
+                try:
+                    total += Tweet(url.tweetid, url.user, self.auth.api).compute_audience()
+                except TweepError:
+                    break
+        return total
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    stats = {}
     if request.method == 'POST' and "urls" in request.values:
-        urls = request.values["urls"].split('\n')
+        urls = [u.strip() for u in request.values["urls"].split('\n')]
+        print urls
         auth = AuthHandler(CONSUMER_KEY, CONSUMER_SECRET, USER_KEY, USER_SECRET)
-        urls = [u for u in map(parse_url, urls) if u]
-        stats, error = process(auth, urls)
-        stats["users"] = zip(stats["users"]["screen_name"], stats["users"]["followers"])
-        print stats
-        nurl = {"total": len(urls), "done": len(stats["users"])}
-        return render_template('index.html', stats=stats, error=error, nurl=nurl)
-    return render_template('index.html', stats = {}, error=False)
+        stats["total"] = Process(urls, auth).run()
+    return render_template('index.html', stats=stats)
 
 
 def main():
